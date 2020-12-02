377d7ec10f387ce7e5b77a94d3fc7b688e34ed20
Pete-Scale <petescale@gmail.com>
2020-12-02 13:23:20 -0600

    added question array to js and uncommented .hide

---
+++ script.js
@@ -0,0 +1,47 @@
+var allQuestions = [
+    {
+        question: "What does HTML stand for?",
+        choices:  ["Hypertext Markup Link", "Hyperlink Text Markdown Language", "Hypertext Markup Language", "Handle These Mean Letters"],
+        answer: "Hypertext Markup Language"
+    },
+    {
+        question: "What does DOM stand for?",
+        choices:  ["Document Object Mantle", "Document Object Model", "Deadly Orangutan Machine", "Div Organiser Mode"],
+        answer: "Document Object Model"
+    },
+    {
+        question: "Which of these is not a semantic HTML element?",
+        choices:  ["<main>", "<footer>", "<section>", "<span>"],
+        answer: "<span>"
+    },
+    {
+        question: "What does CSS stand for?",
+        choices:  ["Cool Sexy Styles", "Converging Style Sheets", "Cascading Style Sheets", "Cascading Script Styles"],
+        answer: "Cascading Style Sheets"
+    },
+    {
+        question: "What are the CSS Box Model components in order from the innermost to the outermost part?",
+        choices:  ["Content, Padding, Margin, Border", "Content, Padding, Border, Margin", "Condiment, Pudding, Margarine, Butter", "Content, Margin, Border, Padding"],
+        answer: "Content, Padding, Border, Margin"
+    },
+    {
+        question: "Which of these is targeting the class 'rich-people'?",
+        choices:  ["#rich-people", ".richPeople", "#bill-gates", ".rich-people"],
+        answer: ".rich-people"
+    },
+    {
+        question: "What does JS stand for?",
+        choices:  ["Java Script", "Just Sayin", "JavaScript", "Java-Script"],
+        answer: "JavaScript"
+    },
+    {
+        question: "Which of the choices grabs the number 8 from this array? var boo = [1, 2, 3, 4, 5, 6, 7, 8];",
+        choices:  ["boo[8];", "var boo = [8];", "boo[7];", "boo[hoo];"],
+        answer: "boo[7];"
+    },
+    {
+        question: "How would I call this function? function quiz(status) {return 'Yes it is ' + status + '!';}",
+        choices:  ["quiz('over');", "quiz[over];", "function quiz('over')", "Yes it is over!"],
+        answer: "quiz('over');"
+    }
+]
